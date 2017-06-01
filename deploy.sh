7b16304eae1be42fa2e57bfe30b465210056dd9c
Grayson Gilmore <gilmoreg@live.com>
2017-06-01 14:14:29 -0500

    Docker/travis devops

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e # Abort script at first error
+set -u # Disallow unset variables
+
+# Only run when not part of a pull request and on the master branch
+if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH != "master" ]
+then
+    echo "Skipping deployment on branch=$TRAVIS_BRANCH, PR=$TRAVIS_PULL_REQUEST"
+    exit 0;
+fi
+
+# Deploy docker image to Heroku
+
+# Install the toolbelt, and the required plugin.
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install heroku-container-registry
+docker login -u=$DOCKER_USERNAME -p=$HEROKU_API_KEY registry.heroku.com
+heroku container:push --app pick
