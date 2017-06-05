51678d95fbde7b50b4ec3317a1ca755f90c93a27
Grayson Gilmore <gilmoreg@live.com>
2017-06-05 12:46:20 -0500

    429 error handling

--- assets/javascript/poll.js
+++ assets/javascript/poll.js
@@ -1,7 +1,18 @@
-/* globals $, $$, window */
+/* globals $, $$, window, fetch */
 require('./bling.js');
 
 const Poll = (() => {
+  const apiCall = (url, body) =>
+    fetch(url, {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(res => res.json());
+
   const closeModal = () => {
     const modal = $('.is-active');
     if (modal) modal.classList.remove('is-active');
@@ -16,6 +27,23 @@ const Poll = (() => {
     if (modal) modal.classList.add('is-active');
   };
 
+  const vote = function () { // eslint-disable-line func-names
+    this.classList.add('is-loading');
+    const id = $('.is-active').id.split('-')[1];
+    apiCall(`${window.location.pathname}/vote`, { id })
+    .then(() => {
+      window.location.assign(window.location.pathname);
+    })
+    .catch((err) => {
+      this.classList.remove('is-loading');
+      // Have we voted already?
+      if (err.includes('429')) {
+        // TODO error
+      }
+      console.error(err);
+    });
+  };
+
   return {
     init: () => {
       Array.from($$('[data-animeid]')).forEach((li) => {
@@ -26,6 +54,9 @@ const Poll = (() => {
       });
       Array.from($$('.modal-background')).forEach((background) => {
         background.on('click', closeModal);
+      });
+      Array.from($$('.vote-button')).forEach((button) => {
+        button.on('click', vote);
       });
 
       window.addEventListener('keyup', (e) => {
